90671056e836d949cce251927c082db7e9a9cd87
Illbjorn <am@hades.so>
2024-10-23 04:37:22 -0400

    Add Map and Pairs Tests and Benchmarks

--- map_test.go
+++ map_test.go
@@ -10,28 +10,60 @@ import (
 var (
 	// Good Value 1
 	// Just test basic string interpolation.
-	input1  = "The {val1} brown fox {val2} over the lazy {val3}."
-	map1    = map[string]string{"val1": "quick", "val2": "jumped", "val3": "dog"}
-	output1 = "The quick brown fox jumped over the lazy dog."
+	mapInput1  = "The {val1} brown fox {val2} over the lazy {val3}."
+	mapValues1 = map[string]string{"val1": "quick", "val2": "jumped", "val3": "dog"}
+	mapOutput1 = "The quick brown fox jumped over the lazy dog."
+
 	// Good Value 2
 	// Testing interpolation token escaping.
-	input2  = `The \{quick} brown fox \{jumped} over the lazy \{dog}.`
-	map2    = map[string]string{"quick": "fast", "jumped": "leaped", "dog": "bear"}
-	output2 = "The {quick} brown fox {jumped} over the lazy {dog}."
+	mapInput2  = `The \{quick} brown fox \{jumped} over the lazy \{dog}.`
+	mapValues2 = map[string]string{"quick": "fast", "jumped": "leaped", "dog": "bear"}
+	mapOutput2 = "The {quick} brown fox {jumped} over the lazy {dog}."
+
+	// Good Value 3
+	// Interpolation token present for which there is no `pair` value to replace
+	// it with.
+	mapInput3  = "The {quick} brown fox {jumped} over the lazy {dog}."
+	mapValues3 = map[string]string{"quick": "fast", "jumped": "leaped"}
+	mapOutput3 = "The fast brown fox leaped over the lazy dog."
+
+	// Good Value 4
+	// Tests the early return (no values to interpolate) code path.
+	mapInput4  = "The {quick} brown fox {jumped} over the lazy {dog}."
+	mapValues4 = map[string]string{}
+	mapOutput4 = "The {quick} brown fox {jumped} over the lazy {dog}."
 )
 
 func TestMap(t *testing.T) {
 	// Good Value 1
-	if res := fstr.Map(input1, map1); res != output1 {
-		println("Input1 did not produce expected results.")
-		println(res)
+	if res := fstr.Map(mapInput1, mapValues1); res != mapOutput1 {
+		t.Error("Map input 1 did not produce expected results.")
+		t.Error("Expected:", mapOutput1)
+		t.Error("Received:", res)
 		t.Fail()
 	}
 
 	// Good Value 2
-	if res := fstr.Map(input2, map2); res != output2 {
-		println("Input2 did not produce expected results.")
-		println(res)
+	if res := fstr.Map(mapInput2, mapValues2); res != mapOutput2 {
+		t.Error("Map input 2 did not produce expected results.")
+		t.Error("Expected:", mapOutput2)
+		t.Error("Received:", res)
+		t.Fail()
+	}
+
+	// Good Value 3
+	if res := fstr.Map(mapInput3, mapValues3); res != mapOutput3 {
+		t.Error("Map input 3 did not produce expected results.")
+		t.Error("Expected:", mapOutput3)
+		t.Error("Received:", res)
+		t.Fail()
+	}
+
+	// Good Value 4
+	if res := fstr.Map(mapInput4, mapValues4); res != mapOutput4 {
+		t.Error("Map input 4 did not produce expected results.")
+		t.Error("Expected:", mapOutput4)
+		t.Error("Received:", res)
 		t.Fail()
 	}
 }
---
+++ pairs_test.go
@@ -0,0 +1,94 @@
+package fstr_test
+
+import (
+	"testing"
+
+	"github.com/illbjorn/fstr"
+)
+
+var (
+	// Good Value 1
+	// Just a basic interpolation test.
+	pairsInput1  = "The {val1} brown fox {val2} over the lazy {val3}."
+	pairsValues1 = []string{"val1", "quick", "val2", "jumped", "val3", "dog"}
+	pairsOutput1 = "The quick brown fox jumped over the lazy dog."
+
+	// Good Value 2
+	// Testing interpolation token escaping.
+	pairsInput2  = `The \{quick} brown fox \{jumped} over the lazy \{dog}.`
+	pairsValues2 = []string{"quick", "fast", "jumped", "leaped", "dog", "bear"}
+	pairsOutput2 = "The {quick} brown fox {jumped} over the lazy {dog}."
+
+	// Good Value 3
+	// Interpolation token present for which there is no `pair` value to replace
+	// it with.
+	pairsInput3  = "The {quick} brown fox {jumped} over the lazy {dog}."
+	pairsValues3 = []string{"quick", "fast", "jumped", "leaped"}
+	pairsOutput3 = "The fast brown fox leaped over the lazy dog."
+
+	// Good Value 4
+	// Tests the early return (no values to interpolate) code path.
+	pairsInput4  = "The {quick} brown fox {jumped} over the lazy {dog}."
+	pairsValues4 = []string{}
+	pairsOutput4 = "The {quick} brown fox {jumped} over the lazy {dog}."
+
+	// Bad Value 1
+	// Uneven number of pairs (key with no value or vice versa).
+	pairsInput5  = `The {quick} brown fox {jumped} over the lazy {dog}.`
+	pairsValues5 = []string{"quick", "fast", "jumped", "leaped", "dog"}
+	pairsOutput5 = "The fast brown fox leaped over the lazy dog."
+)
+
+func TestPairs(t *testing.T) {
+	// Good Value 1
+	if res := fstr.Pairs(pairsInput1, pairsValues1...); res != pairsOutput1 {
+		t.Error("Pairs input 1 did not produce expected results.")
+		t.Error("Expected:", pairsOutput1)
+		t.Error("Received:", res)
+		t.Fail()
+	}
+
+	// Good Value 2
+	if res := fstr.Pairs(pairsInput2, pairsValues2...); res != pairsOutput2 {
+		t.Error("Pairs input 2 did not produce expected results.")
+		t.Error("Expected:", pairsOutput2)
+		t.Error("Received:", res)
+		t.Fail()
+	}
+
+	// Good Value 3
+	if res := fstr.Pairs(pairsInput3, pairsValues3...); res != pairsOutput3 {
+		t.Error("Pairs input 3 did not produce expected results.")
+		t.Error("Expected:", pairsOutput3)
+		t.Error("Received:", res)
+		t.Fail()
+	}
+
+	// Good Value 4
+	if res := fstr.Pairs(pairsInput4, pairsValues4...); res != pairsOutput4 {
+		t.Error("Pairs input 4 did not produce expected results.")
+		t.Error("Expected:", pairsOutput4)
+		t.Error("Received:", res)
+		t.Fail()
+	}
+
+	// Bad Value 1
+	if res := fstr.Pairs(pairsInput5, pairsValues5...); res != pairsOutput5 {
+		t.Error("Pairs input 5 did not produce expected results.")
+		t.Error("Expected:", pairsOutput5)
+		t.Error("Received:", res)
+		t.Fail()
+	}
+}
+
+var (
+	// Benchmark Pairs Template
+	benchPairsInput  = `The {val1} brown {val2} jumped over the lazy {val3}.`
+	benchPairsValues = []string{"val1", "quick", "val2", "fox", "val3", "dog"}
+)
+
+func BenchmarkPairs(b *testing.B) {
+	for range b.N {
+		fstr.Pairs(benchPairsInput, benchPairsValues...)
+	}
+}
